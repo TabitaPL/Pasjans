1e99775232fb7e03af386ab4fad303545a54862b
Agnieszka Panek <a.bieniek.pl@gmail.com>
2014-09-17 13:43:06 +0200

    Resizeable window with green background is created.

--- Angel-3.2/Code/Solitaire/main.cpp
+++ Angel-3.2/Code/Solitaire/main.cpp
@@ -14,15 +14,21 @@ int main()
     //		bool antiAliasing		default: false
     //		bool fullScreen			default: false
     //		bool resizable			default: false
-    theWorld.Initialize();
+    theWorld.Initialize(1024, 768, "Solitaire", false, false, true);
 
     //adds the default grid so you can more easily place Actors
-    theWorld.Add(new GridActor(), -1);
 
     //YOUR GAME SETUP CODE HERE
+    Actor *background = new Actor();
+    background->SetColor(0.0f, 0.60f, 0.16f);
+    background->SetSize(100.0f);
+    background->SetDrawShape(ADS_Square);
+    theWorld.Add(background);
 
     // do all your setup first, because this function won't return until you're exiting
     theWorld.StartGame();
+
+    sysLog.Log("Start of the game");
 
     // any cleanup can go here
     theWorld.Destroy();
