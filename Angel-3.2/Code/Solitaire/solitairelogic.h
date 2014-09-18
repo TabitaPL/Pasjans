26c2277f4fcafdfedae2caa53587e63372e0a278
Marek Panek <marek_panek@writeme.com>
2014-09-18 13:46:40 +0200

    Fixed indentation

--- Angel-3.2/Code/Solitaire/solitairelogic.h
+++ Angel-3.2/Code/Solitaire/solitairelogic.h
@@ -5,40 +5,40 @@
 
 struct Card
 {
-  enum class Type {
-    CLUB,
-    HEART,
-    SPADE,
-    DIAMOND
-  };
+    enum class Type {
+        CLUB,
+        HEART,
+        SPADE,
+        DIAMOND
+    };
 
-  enum class Value {
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    JACK,
-    QUEEN,
-    KING,
-    ACE
-  };
-  Type type;
-  Value value;
-};
+    enum class Value {
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING,
+        ACE
+    };
+
+    Type type;
+    Value value;
+    };
 
 class SolitaireLogic
 {
 public:
-  SolitaireLogic();
+    SolitaireLogic();
 
 private:
-  std::vector<Card> m_cards[4];
-
+    std::vector<Card> m_cards[4];
 };
 
 #endif // SOLITAIRELOGIC_H
