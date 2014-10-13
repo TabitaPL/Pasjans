d6b3c14a0a7f7cc6dac004da26b79aab746b8ad2
Agnieszka Panek <a.bieniek.pl@gmail.com>
2014-10-13 19:04:37 +0200

    Set the project for VS2013 on Windows 7. Solution compiles and runs. Changes in source files are mostly line ending related.

--- Angel-3.2/Code/Solitaire/AngelHeader.h
+++ Angel-3.2/Code/Solitaire/AngelHeader.h
@@ -1,14 +1,18 @@
 #ifndef ANGELHEADER_H
 #define ANGELHEADER_H
 
+#if defined(__GCC__)
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #pragma GCC diagnostic ignored "-Wunused-variable"
 #pragma GCC diagnostic ignored "-Wreturn-type"
 #pragma GCC diagnostic ignored "-Wignored-qualifiers"
+#endif
 #include <Angel.h>
+#if defined(__GCC__)
 #pragma GCC diagnostic warning "-Wunused-parameter"
 #pragma GCC diagnostic warning "-Wunused-variable"
 #pragma GCC diagnostic warning "-Wreturn-type"
 #pragma GCC diagnostic warning "-Wignored-qualifiers"
+#endif
 
 #endif // ANGELHEADER_H
